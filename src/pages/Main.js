5ee039f792b09a66a002024be4008ccfa719a932
RecordOfJun <pritty0731@naver.com>
2023-02-05 21:44:28 +0900

    [ADD]디자인

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,43 @@
+import "./Main.css";
+import mainImg from "../images/banner01.png";
+import youtubeLink from "../images/utub.png";
+import autoImg from "../images/auto_system.png";
 const Main=()=>{
     return(
-        <h1>Alnico</h1>
+        <div>
+        <div className="top_bar">
+            {/* <img src="" */}
+        </div>
+        <div className="main_container">
+
+            <img src={mainImg}style={{width:"100%", height:"800px"}}/>
+
+            <div className="youtube_banner">
+                <a href="https://www.youtube.com/channel/UCSUW_pGH6ru65irfT4I7sog" target="_blank">
+                    <div className="youtube_image">
+                        <img src={youtubeLink} style={{width:"700px"}}/>
+                    </div>
+                </a>
+            </div>
+
+            <div className="alnico_product">
+                
+                <p style={{fontSize:"50px", fontWeight:"500",fontFamily:"맑은 고딕",marginTop:"100px"}}>Alnico Product</p>
+
+                <div className="product_content1">
+                    <div className="product_content1-1">
+                        hello
+                    </div>
+                    <div className="product_content1-2">
+                        <div className="auto_image">
+                            {/* <img src={autoImg} style={{width:"480px", height:"250px"}} /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        </div>
     );
 };
 export default Main;
