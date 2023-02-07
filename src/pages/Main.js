269921896615f04ad9e831886119a2cb15089c87
RecordOfJun <pritty0731@naver.com>
2023-02-07 16:28:54 +0900

    [ADD]css,image 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,43 +1,67 @@
 import "./Main.css";
 import mainImg from "../images/banner01.png";
 import youtubeLink from "../images/utub.png";
-import autoImg from "../images/auto_system.png";
 const Main=()=>{
     return(
-        <div>
-        <div className="top_bar">
-            {/* <img src="" */}
-        </div>
-        <div className="main_container">
 
-            <img src={mainImg}style={{width:"100%", height:"800px"}}/>
 
-            <div className="youtube_banner">
-                <a href="https://www.youtube.com/channel/UCSUW_pGH6ru65irfT4I7sog" target="_blank">
-                    <div className="youtube_image">
-                        <img src={youtubeLink} style={{width:"700px"}}/>
-                    </div>
-                </a>
-            </div>
-
-            <div className="alnico_product">
+            <div className="main_container">
+                <div className="top_bar">
                 
-                <p style={{fontSize:"50px", fontWeight:"500",fontFamily:"맑은 고딕",marginTop:"100px"}}>Alnico Product</p>
+                </div>
 
-                <div className="product_content1">
-                    <div className="product_content1-1">
-                        hello
-                    </div>
-                    <div className="product_content1-2">
-                        <div className="auto_image">
-                            {/* <img src={autoImg} style={{width:"480px", height:"250px"}} /> */}
+                <img src={mainImg}style={{width:"100%", height:"800px"}}/>
+
+                <div className="youtube_banner">
+                    <a href="https://www.youtube.com/channel/UCSUW_pGH6ru65irfT4I7sog" target="_blank">
+                        <div className="youtube_image">
+                            <img src={youtubeLink} className="youtube_link"/>
                         </div>
+                    </a>
+                </div>
+
+                <div className="alnico_product">
+                    
+                    <p style={{fontSize:"50px", fontWeight:"500",fontFamily:"맑은 고딕",marginTop:"100px"}}>Alnico Product</p>
+
+                    <div className="product_content">
+
+                        <div className="product_content_left">
+                        </div>
+
+                        <div className="product_content_right">
+                            <div className="auto_system_image">
+                            </div>
+                            <p className="font_top">공장 자동화 시스템</p>
+                            <p className="font_bottom">이송부터 측량, 데이터 관리까지 한 번에 가능합니다.</p>
+                        </div>
+
+                    </div>
+
+                    <div className="product_content">
+
+                        <div className="product_content_left">
+                            <div className="pack_system_image">
+                            </div>
+                            <p className="font_top">포장 시스템</p>
+                            <p className="font_bottom">단시간에 깔끔하고 완벽한 포장</p>
+                        </div>
+
+                        <div className="product_content_right">
+                            <div className="">
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div style={{width:"100%",height:"500px",marginTop:"100px",display:"flex",flexDirection:"column"}}>
+                        <div className="label_print_image"></div>
+                        <p className="font_top">라벨 프린터</p>
+                        <p className="font_bottom">알리코에서는 라벨 프린터기 판매 뿐만 아니라 A/S 서비스까지 제공합니다.</p>
                     </div>
                 </div>
+                
             </div>
-            
-        </div>
-        </div>
     );
 };
 export default Main;
